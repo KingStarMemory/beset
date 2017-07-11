667fc4494e0a27a7e5bd6e32af58e7dd8efa5424
zhangzhuo <237794193@qq.com>
2017-07-11 09:09:45 +0800

    ref功能演示

---
+++ src/components/RefTest.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router'
+
+
+class RefTest extends React.Component{
+	
+	
+	tets(){
+		console.log("222");
+	}
+	
+	
+	
+	render() {
+		return (
+			<div>
+        		<h2>RefTest</h2>
+      		</div>
+		)
+	}
+}
+
+export default RefTest;
