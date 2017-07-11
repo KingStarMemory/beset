e8d30adc4ace21faf8339fcfe25ed91525c095ef
zhangzhuo <237794193@qq.com>
2017-07-11 09:13:54 +0800

    ”新增ref演示功能

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router'
-
+import RefTest from './RefTest.jsx';
 
 class About extends React.Component{
+	
+	refTest(){
+		this.refs.test.tets();
+	}
+	
 	render() {
 		return (
 			<div>
+				<RefTest ref="test" />
+				<button onClick={this.refTest.bind(this)}>test</button>
         		<h2>Techgogogo</h2>
           		他山之石，可以攻玉。主要分享海外最实用的创业，产品，创意，科技，技术等原创原译文章，以助你事业路上一路飞翔！虎嗅，搜狐自媒体，36氪，人人都是产品经理，经理人分享等媒体撰稿人。但，这里才是我们的大本营！
       		</div>
